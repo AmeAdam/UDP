27a8557058e26a515c4f755c8154143eef9527a0
Adam Baszynski <adam.baszynski@gmail.com>
2021-01-18 10:39:55 +0100

    udp listener

--- UdpListener/Program.cs
+++ UdpListener/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Text;
 
 namespace UdpListener
 {
@@ -6,7 +10,32 @@ namespace UdpListener
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var subscription = UdpStream().Select(Deserialize).Subscribe(MessageReceived);
+            Console.ReadLine();
+        }
+        private static void MessageReceived(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static string Deserialize(UdpReceiveResult packet)
+        {
+            var txt = Encoding.ASCII.GetString(packet.Buffer);
+            return string.Format($"Received: {txt} from {packet.RemoteEndPoint.Address} at {DateTime.Now}");
+        }
+
+        private static IObservable<UdpReceiveResult> UdpStream()
+        {
+            return Observable.Using(
+                () =>
+                {
+                    var listener = new UdpClient(9999);
+                    listener.JoinMulticastGroup(IPAddress.Parse("224.0.0.1"));
+                    return listener;
+                },
+                udpClient =>
+                    Observable.Defer(() => Observable.FromAsync(udpClient.ReceiveAsync)).Repeat()
+            );
         }
     }
 }
